e643c6032c08c211362c4ccef5d7f3fd1787c096
Miller Camacho <millercamacho@unicomfacauca.edu.co>
2024-06-07 09:21:49 -0500

    first commit

---
+++ myCalculator.py
@@ -0,0 +1,10 @@
+#Calculadora Desarrollada por Miller Steven Camacho
+while True:
+     print("""Opciones
+    Ingrese '+' para sumar dos numeros
+    Ingrese '-' para restar dos numeros
+    Ingrese '*' para multiplicar dos numeros
+    Ingrese '/' para dividir dos numeros
+    Ingrese 'salir' para finalizar""")
+
+   # teclado=input(":")
