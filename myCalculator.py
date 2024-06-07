d38324493dca6c43ba697c8dbb982a8fb5f04a02
Miller Camacho <millercamacho@unicomfacauca.edu.co>
2024-06-07 09:48:14 -0500

    se Añadio Bienvenida y cambios

--- myCalculator.py
+++ myCalculator.py
@@ -1,10 +1,27 @@
 #Calculadora Desarrollada por Miller Steven Camacho
+
 while True:
-     print("""Opciones
+     
+    print("""Bienvenido
+    Opciones
     Ingrese '+' para sumar dos numeros
     Ingrese '-' para restar dos numeros
     Ingrese '*' para multiplicar dos numeros
     Ingrese '/' para dividir dos numeros
     Ingrese 'salir' para finalizar""")
 
-   # teclado=input(":")
+    teclado=input(":")
+    if teclado=="salir" or teclado=="Salir":
+        break
+    elif teclado=="+":
+        num1=float(input("Ingrese un numero:"))
+        num2=float(input("Ingrese otro numero:"))
+        result=str(num1+num2)
+        print("Respuesta: "+result)
+    elif teclado=="-":
+        num1=float(input("Ingrese un numero:"))
+        num2=float(input("Ingrese otro numero:"))
+        result=str(num1-num2)
+        print("Respuesta: "+result)
+    elif teclado=="*":
+        num1=float(input("Ingrese un numero:"))
